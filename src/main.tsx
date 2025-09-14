62bdfac3c79295f86cda5458eb89e7e983272920
Prajapati-ankit <ankitprajapati10027@outlook.com>
2025-09-15 02:12:33 +0530

    feat: remove ScrollProvider wrapper from App component for simplified rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'           // Tailwind directives
+import './index.css' // Tailwind directives
 import App from './App'
+
 import { StyledEngineProvider } from '@mui/material/styles'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import muiTheme from './theme/muiTheme'
-import ScrollProvider from './providers/ScrollProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
-        <ScrollProvider>
-          <App />
-        </ScrollProvider>
+        <App /> {/* ✅ Router now lives inside App */}
       </ThemeProvider>
     </StyledEngineProvider>
   </React.StrictMode>
